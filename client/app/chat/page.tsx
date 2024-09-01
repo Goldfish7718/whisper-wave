fda9aecc7d97f037324635b9064006afad8fddde
Tejas <tejasnanoti2@gmail.com>
2024-09-01 12:01:59 +0530

    layout: Complete main chat interface;

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import React from 'react'
 import contacts from '@/data/contacts.json'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { AudioWaveform } from 'lucide-react'
 import ContactCard from '@/components/ContactCard'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import chatData from '@/data/chatData.json'
 
 const Chat = () => {
   return (
@@ -19,10 +23,38 @@ const Chat = () => {
         </aside>
 
         {/* CHAT INTERFACE */}
-        <section className='ml-0 sm:ml-[300px] md:ml-[400px] flex justify-center items-center h-screen bg-neutral-100 dark:bg-neutral-900'>
-            <div className='fixed flex flex-col items-center gap-1'>
-                <AudioWaveform size={28} className='text-neutral-500' />
-                <h3 className='text-neutral-500 font-medium text-lg'>Select a conversation to get started</h3>
+        <section className='sm:ml-[300px] md:ml-[400px]'>
+
+            {/* NO CONVERWSATION SELECTED */}
+            {/* <div className='flex justify-center items-center h-screen bg-neutral-100 dark:bg-neutral-900'>
+                <div className='fixed flex flex-col items-center gap-1'>
+                    <AudioWaveform size={28} className='text-neutral-500' />
+                    <h3 className='text-neutral-500 font-medium text-lg'>Select a conversation to get started</h3>
+                </div>
+            </div> */}
+
+            {/* CONVERSATION SELECTED */}
+            <div className='h-screen'>
+                <div className='fixed flex items-center p-2 gap-2 border-b-[1px] border-neutral-300 dark:border-neutral-800 w-full z-10 backdrop-blur-sm'>
+                    <Avatar className='h-8 w-8'>
+                        <AvatarImage src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8f94f7d9-b68a-445e-a003-eceb64087873/dcsohgg-6068e5ed-b130-4e90-9d0e-2818ffef14bb.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhmOTRmN2Q5LWI2OGEtNDQ1ZS1hMDAzLWVjZWI2NDA4Nzg3M1wvZGNzb2hnZy02MDY4ZTVlZC1iMTMwLTRlOTAtOWQwZS0yODE4ZmZlZjE0YmIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dxqnkfCNycNrkqzRpEXcx_7Py_LuhvSDunFDufPyBDM" alt="CN" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+
+                    <h3>John Doe</h3>
+                </div>
+
+                <div className='py-12 h-full'>
+                    <ScrollArea className='h-full'>
+                            {chatData.map((chatBlock, index) => (
+                                <div className={`m-2  ${index == chatData.length - 1 ? 'mb-12' : ''}`}>
+                                    {chatBlock.messages.map(message => (
+                                        <div className={`p-2 m-1 bg-neutral-100 w-2/3 ${index % 2 == 0 ? 'ml-auto' : ''}`}>{message}</div>
+                                    ))}
+                                </div>
+                            ))}
+                    </ScrollArea>
+                </div>
             </div>
         </section>
     </>
