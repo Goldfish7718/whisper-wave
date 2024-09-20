ca5c518151373746112055ea42c067f674470176
Tejas <tejasnanoti2@gmail.com>
2024-09-20 20:43:04 +0530

    fix: Fix copy button clipping out of viewport in Profile Drawer;

--- client/components/ProfileTrigger.tsx
+++ client/components/ProfileTrigger.tsx
@@ -90,30 +90,28 @@ const ProfileTrigger = ({ children }: TriggerProps) => {
       <Drawer open={open} onOpenChange={setOpen}>
         <DrawerTrigger asChild>{children}</DrawerTrigger>
         <DrawerContent>
-          <div className="p-4">
-            <div className="flex gap-8 items-center">
-              <Avatar className="h-16 w-16">
-                <AvatarImage
-                  src={user?.imageUrl}
-                  alt={user?.fullName as string}
-                />
-                <AvatarFallback>
-                  {user?.fullName && getInitials(user?.fullName)}
-                </AvatarFallback>
-              </Avatar>
+          <div className="flex gap-8 items-center p-4">
+            <Avatar className="h-16 w-16">
+              <AvatarImage
+                src={user?.imageUrl}
+                alt={user?.fullName as string}
+              />
+              <AvatarFallback>
+                {user?.fullName && getInitials(user?.fullName)}
+              </AvatarFallback>
+            </Avatar>
 
-              <div>
-                <h3>{user?.fullName}</h3>
-                <div className="flex gap-4 items-center">
-                  <span className="text-neutral-500">{user?.id}</span>
-                  <Button
-                    size="sm"
-                    variant="outline"
-                    className="p-2 text-neutral-500 text-[12px]"
-                    onClick={handleCopy}>
-                    <Clipboard size={14} />
-                  </Button>
-                </div>
+            <div>
+              <h3>{user?.fullName}</h3>
+              <div className="flex flex-col gap-1 justify-start my-2">
+                <span className="text-neutral-500">{user?.id}</span>
+                <Button
+                  size="sm"
+                  variant="outline"
+                  className="p-2 text-neutral-500 text-[12px] w-fit"
+                  onClick={handleCopy}>
+                  <Clipboard size={14} />
+                </Button>
               </div>
             </div>
           </div>
